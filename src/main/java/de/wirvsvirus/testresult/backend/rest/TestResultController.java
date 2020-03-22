281574e65bcc1c2fe16f7a9f01cb52d88185ee0c
Thomas <yddrass@gmail.com>
2020-03-22 16:10:49 +0100

    no data now results in 404

--- src/main/java/de/wirvsvirus/testresult/backend/rest/TestResultController.java
+++ src/main/java/de/wirvsvirus/testresult/backend/rest/TestResultController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import de.wirvsvirus.testresult.backend.exceptions.FalseInformedException;
+import de.wirvsvirus.testresult.backend.exceptions.NoDataException;
 import de.wirvsvirus.testresult.backend.model.TestResult;
 import de.wirvsvirus.testresult.backend.service.TestResultPushService;
 import de.wirvsvirus.testresult.backend.service.TestResultService;
@@ -29,8 +30,8 @@ public class TestResultController {
 	private TestResultPushService pushService;
 
 	@GetMapping("/{id}")
-	public Optional<TestResult> getTestResult(@PathVariable("id") String id) {
-		return testResultService.getTestResult(id);
+	public TestResult getTestResult(@PathVariable("id") String id) throws NoDataException {
+		return testResultService.getTestResult(id).orElseThrow(NoDataException::new);
 
 	}
 
@@ -43,7 +44,6 @@ public class TestResultController {
 		if (!previousResultOptional.isPresent()) {
 			return informNegatives(testResult);
 		} else {
-			
 			TestResult previousResult = previousResultOptional.get();
 			if(previousResult.getStatus() == testResult.getStatus() ) {
 				if( previousResult.isNotified()) {
