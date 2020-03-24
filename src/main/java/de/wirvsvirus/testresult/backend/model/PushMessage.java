cb86a09d95ddac6fec2ecf93c151b3324b92bc6e
Jules <jul.sandrock@gmail.com>
2020-03-24 12:01:37 +0100

    add general constructor and getters to PushMessage

--- src/main/java/de/wirvsvirus/testresult/backend/model/PushMessage.java
+++ src/main/java/de/wirvsvirus/testresult/backend/model/PushMessage.java
@@ -8,5 +8,24 @@ public class PushMessage {
 	private String contact;
 	private String text;
 	private String from;
+
+
+	PushMessage(String contact, String text, String from) {
+		this.contact = contact;
+		this.text = text;
+		this.from = from;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public String getFrom() {
+		return from;
+	}
 	
 }
